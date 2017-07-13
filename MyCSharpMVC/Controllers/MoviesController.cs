a86ec25042438ac2f43c6abcc893ee19b18f533a
Imran Artwel <imranartweljunior@gmail.com>
2017-07-13 10:55:37 +0800

    initial commit-with list(movies,customers)

---
+++ MyCSharpMVC/Controllers/MoviesController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using MyCSharpMVC.Models;
+using MyCSharpMVC.ViewModels;
+
+
+namespace MyCSharpMVC.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Prison Break" },
+                new Movie { Id = 2, Name = "Lion King" }
+            };
+        }
+
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Imran Artwel" },
+                new Customer { Name = "Piya Artwel" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+    }
+}
